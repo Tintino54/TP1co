114847fa603a24f74864ccca0649128501e242a5
jdefaye <johan.defaye@hotmail.fr>
2016-01-21 11:43:34 +0100

    ajout fonction de tri

--- fonctions.c
+++ fonctions.c
@@ -1,2 +1,23 @@
 #include "fonctions.h"
 
+void sort_by_cost(edge * tab_edge[], unsigned int size_tab_edge) {
+	int i, j;
+	edge tmp;
+	for (i = 0; i < size_tab_edge-1; ++i) {
+		for (j = i+1; j < size_tab_edge; ++j) {
+			if ((*tab_edge)[i].cost > (*tab_edge)[j].cost) {
+				tmp = *tab_edge[i];
+				(*tab_edge)[i] = (*tab_edge)[j];
+				(*tab_edg)e[j] = tmp;
+			}
+		}
+	}
+}
+
+
+edge_list kruskal(node tab_node[], edge tab_edge[], unsigned int size_tab_node, unsigned int size_tab_edge) {
+	edge_list G = NULL;
+	sort_by_cost(&tab_edge, size_tab_edge);
+
+	return G;
+}
--- fonctions.h
+++ fonctions.h
@@ -6,7 +6,7 @@
 
 	/* Structure creation */
 
-struct node {
+struct node{
 	int x_node;
 	int y_node;
 	int id_node;
@@ -14,15 +14,25 @@ struct node {
 typedef struct node node;
 
 
-struct edge {
+struct edge{
 	int node_a;
 	int node_b;
 	double cost;
 };
 typedef struct edge edge;
 
+
+struct element{
+	edge val;
+	edge * suiv;
+};
+typedef struct element * edge_list;
+	
+
 	/* functions creation */
 
+void sort_by_cost(edge tab_edge[], unsigned int size_tab_edge);
 
+edge_list kruskal(node tab_node[], edge * tab_edge[], unsigned int size_tab_node, unsigned int size_tab_edge);
 
 #endif
